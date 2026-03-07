f31da1d1e21aa003804f6ca3af4bcc347e72db2f
eric20150705 <chiangeric0705@gmail.com>
2026-03-07 16:34:57 +0800

    新增 tkinter 應用程式，包含顯示和清除按鈕功能

--- class1/prj02.py
+++ class1/prj02.py
@@ -7,6 +7,12 @@ from tkinter import *
 def butten():
     # 顯示hi在終端機上
     print("hi")
+    display.config(text="歡迎來到2D遊戲", fg="red", bg="black")
+
+
+def clear():
+    # 清除標籤內容，將文字顏色和背景顏色設為白色
+    display.config(text="", fg="white", bg="white")
 
 
 #######################建立視窗########################
@@ -17,13 +23,17 @@ window.title("2D遊戲")
 
 ############################建立按鈕########################
 # 建立按鈕，當按鈕被按下時，會呼叫 butten 函數
-btn1 = Button(window, text="按我", command=butten)
+btn1 = Button(window, text="show", command=butten)
 # 將按鈕加入視窗中
 btn1.pack()
+# 建立按鈕，當按鈕被按下時，會呼叫 butten 函數
+btn2 = Button(window, text="clean", command=clear)
+# 將按鈕加入視窗中
+btn2.pack()
 #########################建立標籤########################
 # 建立標籤，顯示歡迎訊息，文字顏色為紅色，背景顏色為黑色
 # Label參數說明：(視窗名稱,文字內容,前景顏色,背景顏色)
-display = Label(window, text="歡迎來到2D遊戲", fg="red", bg="black")
+display = Label(window, text="")
 
 # 將標籤加入視窗中
 display.pack()
